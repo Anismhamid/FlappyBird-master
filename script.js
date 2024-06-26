68b3fc9d2d3cfaa896e07cd52c079569bb5d7ac6
Anis Mhamid <anesmhamed1@gmail.com>
2024-06-26 19:00:21 +0300

    init

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ initializeHole()
 
 
 hole.addEventListener('animationiteration', () => {
-    let random = -((Math.random() * 300) + 150);
+    let random = -((Math.random() * 600) + 200);
     hole.style.top = random + "px";
     counter++;
 
@@ -16,20 +16,42 @@ hole.addEventListener('animationiteration', () => {
 });
 
 
-
-
 setInterval(function () {
+    let array = []
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if (jumping == 0) {
         character.style.top = (characterTop + 3) + "px";
+        array.push(counter)
         document.getElementById('score').innerHTML = `${counter}`;
+
     }
+    for (let i = 0; i < array.length; i++) {
+        if (array[0] == 5) {
+            block.style.animation = 'block 4s linear infinite'
+            hole.style.animation = 'block 4s linear infinite'
+        } else if (array[0] == 10) {
+            block.style.animation = 'block 3s linear infinite'
+            hole.style.animation = 'block 3s linear infinite'
+        }
+        else if (array[0] == 15) {
+            block.style.animation = 'block 2s linear infinite'
+            hole.style.animation = 'block 2s linear infinite'
+        }
+        else if (array[0] == 20) {
+            block.style.animation = 'block 1s linear infinite'
+            hole.style.animation = 'block 1s linear infinite'
+        }
+    }
+
+
+
+
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
     let cTop = -(800 - characterTop);
-    if ((characterTop > 780) || ((blockLeft < 100) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 130)))) {
+    if ((characterTop > 860) || ((blockLeft < 100) && (blockLeft > -0) && ((cTop < holeTop) || (cTop > holeTop -100)))) {
         alert("Game over. Score: " + (counter - 1));
-        document.getElementById('char-img').src = 'gameImages/bird2.png'
+        document.getElementById('char-img').src = 'gameImages/bird3.png'
         character.style.top = 100 + "px";
         counter = 0;
         initializeHole()
@@ -43,16 +65,16 @@ function jump() {
     let jumpInterval = setInterval(function () {
         let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
         if ((characterTop > 6) && (jumpCount < 15)) {
-            character.style.top = (characterTop - 5) + "px";
-            document.getElementById('char-img').src = 'gameImages/bird3.png'
+            character.style.top = (characterTop - 4) + "px";
+            document.getElementById('char-img').src = 'gameImages/bird2.png'
         }
         if (jumpCount > 20) {
             clearInterval(jumpInterval);
             jumping = 0;
             jumpCount = 0;
-        } 
-        if (jumpCount % 5 === 0) { // Change image every 5 jumps
-            document.getElementById('char-img').src = 'gameImages/bird2.png';
+        }
+        if (jumpCount % 100 === 0) {
+            document.getElementById('char-img').src = 'gameImages/bird3.png';
         }
         jumpCount++;
     }, 10);
@@ -60,6 +82,8 @@ function jump() {
 
 
 function initializeHole() {
-    let random = -((Math.random() * 300) + 150);
+    let random = -((Math.random() * 600) + 200);
     hole.style.top = random + "px";
 }
+
+
